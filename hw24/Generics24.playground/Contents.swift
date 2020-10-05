41cf33ee8170f52abf9862fc22d30a227c7d3065
liza.metla <liza.metla@gmail.com>
2020-10-06 02:26:56 +0300

    Done hw 24 part 2

--- hw24/Generics24.playground/Contents.swift
+++ hw24/Generics24.playground/Contents.swift
@@ -7,34 +7,59 @@ protocol Shoot {
     func shoot()
 }
 
-class Solder<T>{
-    //name
-    //завние
-    //подключить протокол стрелять
-    var name: String
-    var rank: String
-    
-    init(name: String, rank: String) {
-        self.name = name
-        self.rank = rank
+class Solder<T> : Shoot{
+    func shoot() {
+        print("Пиу-пиу " + name + ", " + rank + "\n")
     }
 
-    //класс с шаблоном
-    //вызвать метод протокола
-    //метод называет имя звание
+    var name: String
+    var rank: String
+    var solderLife: Int
+    
+    init(name: String, rank: String, solderLife: Int) {
+        self.name = name
+        self.rank = rank
+        self.solderLife = solderLife
+    }
+
 }
 
 extension Solder  {
-    var solderLife: Int
-    {
+    func checkSolderlife() {
         if solderLife > 10 {
             print("А вот и дембель!")
         } else {
             print("Пусть чистит картошку")
         }
-        return
     }
-    
 }
+
+let solder = Solder<Any>(name: "Sasha", rank: "Solder", solderLife: 5)
+solder.checkSolderlife()
+solder.shoot()
+
+
+var solders = [Solder<Int>]()
+solders.append(Solder(name: "Sasha", rank: "Solder", solderLife: 5))
+solders.append(Solder(name: "Denis", rank: "Solder", solderLife: 3))
+solders.append(Solder(name: "Maxim", rank: "Oficer", solderLife: 25))
+
+var sortSolders = solders.sorted(by: { $0.solderLife > $1.solderLife })
+
+
+print("\nОтсортированные солдаты \n")
+
+for solder in sortSolders {
+    print(solder.name, solder.rank, solder.solderLife)
+}
+
+print("\nУдаление военослужащего с самым большим сроком службы \n")
+sortSolders.remove(at: 0)
+
+for solder in sortSolders {
+    print(solder.name, solder.solderLife)
+}
+
+
 
 
